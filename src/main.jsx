439a738393c66e9b19d714ea45fb4b819a3445c1
raisakuna <sakunaxl@gmail.com>
2024-12-06 21:49:35 -0500

    tailwind and flowbite added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import LandingPage from "./pages/landing/index.jsx";
+import "./assets/css/main.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <LandingPage />
+  </StrictMode>
+);
