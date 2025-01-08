7692202a322e90f72f3de958b79f4d7fd9a0a38c
raisakuna <sakunaxl@gmail.com>
2025-01-08 18:23:35 -0500

    separating inputfileld and textiputfield, creating as a component

---
+++ src/components/form/input.component.jsx
@@ -0,0 +1,61 @@
+import { useController } from "react-hook-form";
+export const InputLabel = ({ field, labelTxt }) => {
+  return (
+    <>
+      <label
+        htmlFor={field}
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        {labelTxt}
+      </label>
+    </>
+  );
+};
+
+export const TextInputField = ({
+  control,
+  type = "text",
+  name,
+  defaultValue = "",
+  errMsg = null,
+  placeholder = "",
+}) => {
+  const { field } = useController({
+    control: control,
+    name: name,
+    defaultValue: defaultValue,
+  });
+  return (
+    <>
+      <input
+        type={type}
+        {...field}
+        className={`
+                bg-gray-50 
+                border 
+                ${errMsg ? "border-red-400" : "border-gray-300"}
+                text-gray-900 
+                rounded-lg 
+                focus:ring-primary-600 
+                focus:border-primary-600 
+                block 
+                w-full 
+                p-2 
+                dark:bg-gray-700
+                 dark:border-gray-600 
+                 dark:placeholder-gray-400 
+                 dark:text-white 
+                 dark:focus:ring-blue-500 
+                 dark:focus:border-blue-500`}
+        placeholder={placeholder}
+      />
+      {errMsg ? (
+        <>
+          <span className="text-sm italic text-red-800">{errMsg}</span>
+        </>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
--- src/pages/auth/login/login.page.jsx
+++ src/pages/auth/login/login.page.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
 import LogoComponent from "../../../components/companylogo/logo.component";
-
+import { useForm } from "react-hook-form";
+import {
+  InputLabel,
+  TextInputField,
+} from "../../../components/form/input.component";
 const LoginPage = () => {
-  const [credentials, setCredentials] = useState();
-
-  const inputChangeEvent = (e) => {
-    const { value, name, type, required } = e.target.value;
-    console.log(value);
-
-    setCredentials({
-      ...credentials,
-      [name]: value,
-    });
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const submitEvent = (data) => {
+    console.log(data);
   };
   return (
     <>
@@ -28,38 +28,31 @@ const LoginPage = () => {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form
+                onSubmit={handleSubmit(submitEvent)}
+                className="space-y-4 md:space-y-6"
+                action="#"
+              >
                 <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Your email
-                  </label>
-                  <input
+                  <InputLabel field={"email"} labelTxt={"Your email"} />
+
+                  <TextInputField
+                    control={control}
+                    name={"email"}
                     type="email"
-                    name="email"
-                    id="email"
-                    onChange={inputChangeEvent}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
-                    required={true}
+                    errMsg={errors?.email?.message}
+                    placeholder="Enter your email..."
                   />
                 </div>
+
                 <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Password
-                  </label>
-                  <input
+                  <InputLabel field={"password"} labelTxt={"Password"} />
+                  <TextInputField
+                    control={control}
+                    name={"password"}
                     type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required={true}
+                    errMsg={errors?.password?.message}
+                    placeholder="Enter your password..."
                   />
                 </div>
                 <div className="flex items-center justify-between">
